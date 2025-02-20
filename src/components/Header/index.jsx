6007c05968be720f2d26123d194d28f44485f7ac
Giulliano <tharsoweb@gmail.com>
2025-02-20 14:48:54 -0300

    estilização do header com autenticação

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,24 +7,25 @@ import { ShoppingCartSimple } from '../../assets/svg/shopping-cart-simple'
 import { Hamburger } from '../../assets/svg/hamburger'
 import { Close } from '../../assets/svg/close'
 import { useState } from 'react'
+import { LogoAlt } from '../../assets/svg/logo-alt'
 
-const Header = () => {
+const Header = ({ authenticated }) => {
 
     const [openMenu, setOpenMenu] = useState(false)
     const [scrolling, setScrolling] = useState(window.scrollY > 0)
-    
+
     const handleOpenMenu = () => setOpenMenu(!openMenu)
 
     window.addEventListener("scroll", () => setScrolling(window.scrollY > 0))
 
     return (
-        <header className={styles.header + (scrolling ? ` ${styles.headerScrolling}` : "")}>
+        <header className={styles.header + (scrolling ? ` ${styles.headerScrolling}` : "") + (authenticated ? ` ${styles.headerAuthenticated}` : "")}>
             <Container className={styles.header__container}>
                 <div className={styles.header__logo}>
                     <h1 className={styles.header__title}>Étoile Bistrò</h1>
 
                     <a href="#">
-                        <Logo />
+                        {authenticated ? <LogoAlt /> : <Logo />}
                     </a>
                 </div>
 
