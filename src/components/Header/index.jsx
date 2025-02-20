75fc02efbde99e5fd1d45b5ba94f474a5f974bf5
Giulliano <tharsoweb@gmail.com>
2025-02-20 13:02:14 -0300

    seção header 3

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,39 +1,48 @@
 import Container from '../Container'
 import styles from './Header.module.css'
-import logo from "../../assets/images/logo.svg"
-import userIcon from "../../assets/images/user-circle.svg"
-import cartIcon from "../../assets/images/shopping-cart-simple.svg"
-import hamburgerIcon from "../../assets/images/hamburger.svg"
-import closeIcon from "../../assets/images/close.svg"
 import MainMenu from '../MainMenu'
+import { Logo } from '../../assets/svg/logo'
+import { UserCircle } from '../../assets/svg/user-circle'
+import { ShoppingCartSimple } from '../../assets/svg/shopping-cart-simple'
+import { Hamburger } from '../../assets/svg/hamburger'
+import { Close } from '../../assets/svg/close'
+import { useState } from 'react'
 
 const Header = () => {
+
+    const [openMenu, setOpenMenu] = useState(false)
+    const [scrolling, setScrolling] = useState(window.scrollY > 0)
+    
+    const handleOpenMenu = () => setOpenMenu(!openMenu)
+
+    window.addEventListener("scroll", () => setScrolling(window.scrollY > 0))
+
     return (
-        <header className={styles.header}>
+        <header className={styles.header + (scrolling ? ` ${styles.headerScrolling}` : "")}>
             <Container className={styles.header__container}>
                 <div className={styles.header__logo}>
                     <h1 className={styles.header__title}>Étoile Bistrò</h1>
 
                     <a href="#">
-                        <img src={logo} alt="Étoile Bistrò" />
+                        <Logo />
                     </a>
                 </div>
 
-                <nav className={styles.navigation}>
+                <nav className={styles.navigation + (openMenu ? ` ${styles.navigationOpenMenu}` : "")}>
                     <div className={styles.navigation__profile}>
                         <a href="#">
-                            <img src={userIcon} alt="Perfil" />
+                            <UserCircle />
                         </a>
                     </div>
 
                     <div className={styles.navigation__cart}>
                         <a href="#">
-                            <img src={cartIcon} alt="Carrinho" />
+                            <ShoppingCartSimple />
                         </a>
                     </div>
 
-                    <div className={styles.navigation__menuIcon}>
-                        <img src={hamburgerIcon} alt="Open Menu" />
+                    <div className={styles.navigation__menuIcon} onClick={handleOpenMenu}>
+                        {openMenu ? <Close /> : <Hamburger />}
                     </div>
 
                     <div className={styles.navigation__overlay}>
