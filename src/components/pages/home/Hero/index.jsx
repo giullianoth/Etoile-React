3e72d6e920b8050a41f028a5547c1e62ac0f789a
Giulliano <tharsoweb@gmail.com>
2025-02-20 13:02:41 -0300

    seção hero

--- src/components/pages/home/Hero/index.jsx
+++ src/components/pages/home/Hero/index.jsx
@@ -1,8 +1,24 @@
+import Container from '../../../Container'
 import styles from './Hero.module.css'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className={styles.hero}>
+      <Container className={styles.hero__container}>
+        <p className={styles.hero__subtitle}>Paixão da nossa cozinha à sua mesa.</p>
+
+        <header className={styles.hero__title}>
+          <h1>Bem-vindo ao Étoile</h1>
+        </header>
+
+        <p className={styles.hero__headline}>Aqui, tradição dança com a criatividade moderna para lhe dar uma experiência culinária única. Cada prato é um abraço de sabor, concebido com amor e dedicação para tornar seu dia inesquecível.</p>
+
+        <div className={styles.hero__cta}>
+          <a href="#" className={`${styles.hero__ctaButton} button primary`}>Faça seu pedido</a>
+          <a href="#" className={`${styles.hero__ctaButton} button secondary`}>Nossos pratos</a>
+        </div>
+      </Container>
+    </section>
   )
 }
 
