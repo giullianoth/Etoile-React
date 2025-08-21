b33e549d2d90ba99721104f408d6a1c625305e38
Giulliano <tharsoweb@gmail.com>
2025-08-21 17:47:38 -0300

    segunda seção de apresentação

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,13 @@
 import Hero from "../../components/pages/home/Hero"
 import Presentation1 from "../../components/pages/home/Presentation1"
+import Presentation2 from "../../components/pages/home/Presentation2"
 
 const Home = () => {
     return (
         <>
             <Hero />
             <Presentation1 />
+            <Presentation2 />
         </>
     )
 }
