a9af59ea70fee571bb4e894bc7d4d7c6df93bbfe
Giulliano Guimarães <llthguimaraes@gmail.com>
2025-09-09 14:07:01 -0300

    componente de loading no app

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Footer from './components/Footer'
-import Header from './components/Header'
-import Home from './pages/Home'
-import Plates from './pages/Plates'
-import Cart from './pages/Cart'
-import Login from './pages/Auth/Login'
-import Register from './pages/Auth/Register'
-import Profile from './pages/Profile'
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
+import "./App.css"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Home from "./pages/Home"
+import Plates from "./pages/Plates"
+import Cart from "./pages/Cart"
+import Login from "./pages/Auth/Login"
+import Register from "./pages/Auth/Register"
+import Profile from "./pages/Profile"
 import Modal from "react-modal"
-import { useAuth } from './hooks/useAuth'
+import { useAuth } from "./hooks/useAuth"
+import Loading from "./components/Loading"
 
 Modal.setAppElement("#root")
 
 function App() {
-  const { authenticated } = useAuth()
+  const { authenticated, loading } = useAuth()
+
+  if (loading) {
+    return (
+      <div className="app-loading">
+        <Loading />
+      </div>
+    )
+  }
 
   return (
     <BrowserRouter>
@@ -22,12 +31,12 @@ function App() {
 
       <main>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/pratos' element={<Plates />} />
-          <Route path='/carrinho' element={<Cart />} />
-          <Route path='/login' element={authenticated ? <Navigate to="/perfil" /> : <Login />} />
-          <Route path='/cadastrar' element={authenticated ? <Navigate to="/perfil" /> : <Register />} />
-          <Route path='/perfil' element={authenticated ? <Profile /> : <Navigate to="/login" />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/pratos" element={<Plates />} />
+          <Route path="/carrinho" element={<Cart />} />
+          <Route path="/login" element={authenticated ? <Navigate to="/perfil" /> : <Login />} />
+          <Route path="/cadastrar" element={authenticated ? <Navigate to="/perfil" /> : <Register />} />
+          <Route path="/perfil" element={authenticated ? <Profile /> : <Navigate to="/login" />} />
         </Routes>
       </main>
 
