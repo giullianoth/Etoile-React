9b05e4337c8f3e633021888041d6fa891c83bd66
Giulliano <llthguimaraes@gmail.com>
2025-11-22 15:30:42 -0300

    refatoração 2: segunda seção de apresentação

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,13 @@
 import Hero from "../../components/Pages/Home/Hero"
 import Presentation from "../../components/Pages/Home/Presentation"
+import Presentation2 from "../../components/Pages/Home/Presentation2"
 
 const Home = () => {
   return (
     <>
       <Hero />
       <Presentation />
+      <Presentation2 />
     </>
   )
 }
